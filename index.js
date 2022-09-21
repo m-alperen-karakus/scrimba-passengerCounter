d5fcd1190ae14a8fec2b22e13e3ac88df2cb3fc7
Mustafa Alperen Karakuş <92369384+m-alperen-karakus@users.noreply.github.com>
2022-09-22 01:58:55 +0300

    asdasd

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+let count=0
+let entries=[0,5]
 
+function increment(){
+    count+=1
+    document.getElementById("count-el") = count
+}
+
+function save(){
+    entries.push(count)
+    document.getElementById("save-el") = count
+
+}
